76aef3e3fdcb5fbaa8f38f3c8920961ff1980ff3
bexster <140751518+bexster@users.noreply.github.com>
2024-10-21 12:04:44 +0900

    added js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function createPoem(event) {
+  event.preventDefault();
+  let displayPoem = document.querySelector("#poem");
+  new Typewriter("#poem", {
+    strings: "creating your unique poem",
+    autoStart: true,
+    delay: 30,
+  });
+}
 
+let createForm = document.querySelector("#create-poem-form");
+createForm.addEventListener("submit", createPoem);
